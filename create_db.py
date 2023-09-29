babeb58443b1ce9cc4d63cef3bbf1abf250ee2a3
VadimMarenko <130446385+VadimMarenko@users.noreply.github.com>
2023-09-29 18:59:58 +0300

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,14 +1,17 @@
 import sqlite3
 from pprint import pprint
 
+database = "./hw_06.db"
+file_sql = "./create_hw_06_db.sql"
 
-def create_db():
+
+def create_db(db_file, sql_file):
     # читаємо файл зі скриптом для створення БД
-    with open("create_hw_06_db.sql", "r") as f:
+    with open(sql_file, "r") as f:
         sql = f.read()
 
     # створюємо з'єднання з БД (якщо файлу з БД немає, він буде створений)
-    with sqlite3.connect("hw_06.db") as con:
+    with sqlite3.connect(db_file) as con:
         cur = con.cursor()
         # виконуємо скрипт із файлу, який створить таблиці в БД
         cur.executescript(sql)
@@ -16,6 +19,6 @@ def create_db():
 
 if __name__ == "__main__":
     try:
-        create_db()
+        create_db(database, file_sql)
     except sqlite3.OperationalError as error:
         pprint(error)
